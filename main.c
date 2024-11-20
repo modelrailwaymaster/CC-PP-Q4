38d1f9aed1598a1ea51de8acbe8abd42c56a9898
Henry Owen <henryowen2006@outlook.com>
2024-11-20 15:31:09 +0000

    second

--- main.c
+++ main.c
@@ -14,20 +14,52 @@ struct file_change
 
 struct file
 {
-    char file_name;
-    char file_directory;
-    char data;
+    char *file_name;
+    char *file_directory;
+    char *data;
     struct file_change change_log[];
 };
+
+struct file current_file;
 
 int open_file()
 {
     bool complete = false;
+    FILE *fp;
+    char input[20];
+    char letter;
+    char *data;
 
     while (!complete)
     {
-        printf("Enter the file name: ");
+        printf("Enter the file name(without .txt): ");
+        scanf("%s", input);
+        fp = fopen(strcat(strcat(directory, input), ".txt"), "r");
+        fseek(fp, 0, SEEK_END);
+        data = (char *)malloc(ftell(fp) * sizeof(char));
+        fseek(fp, 0, SEEK_SET);
+        while (true)
+        {
+            letter = fgetc(fp);
+            if (letter == EOF)
+            {
+                complete = true;
+                break;
+            }
+            else
+            {
+                printf("%s", data);
+                data[ftell(fp)] = letter;
+            }
+        }
+        fclose(fp);
     }
+
+    printf("%s", data);
+    current_file.file_name = input;
+    current_file.file_directory = directory;
+    current_file.data = data;
+    return 0;
 }
 
 int copy()
@@ -47,23 +79,23 @@ int delete()
 
 int show()
 {
+    printf("%s", current_file.data);
     return 0;
 }
 
-int fop_exit()
+int save_file()
 {
-    return -1;
+    return 0;
+}
+
+int delete_file()
+{
+    return 0;
 }
 
 int lineOp()
 {
     printf("lop\n");
-    return 0;
-}
-
-int generalOp()
-{
-    printf("gop\n");
     return 0;
 }
 
@@ -101,12 +133,31 @@ int fileOp()
 {
     bool run = true;
     char *options[5] = {"copy", "create", "delete", "show", "exit"};
-    int (*functions[5])() = {&copy, &create, &delete, &show, &fop_exit};
+    int (*functions[5])() = {&copy, &create, &delete, &show, &my_exit};
     int result;
 
     while (run)
     {
         result = menu(options, functions, 5);
+        if (result == -1)
+        {
+            run = false;
+        }
+    }
+
+    return 0;
+}
+
+int generalOp()
+{
+    bool run = true;
+    char *options[4] = {"open", "save", "delete", "exit"};
+    int (*functions[4])() = {&open_file, &save_file, &delete_file, &my_exit};
+    int result;
+
+    while (run)
+    {
+        result = menu(options, functions, 4);
         if (result == -1)
         {
             run = false;
